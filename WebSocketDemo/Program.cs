7b3f3c3ee4a66e64ca7715bd267f0d4907c6225b
Adam Monikowski <adam.piotr.monikowski@gmail.com>
2024-12-14 13:54:17 +0100

    Initial commit

---
+++ WebSocketDemo/Program.cs
@@ -0,0 +1,64 @@
+using System.Net.WebSockets;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseWebSockets();
+
+app.Use(async (context, next) =>
+{
+    if (context.Request.Path == "/ws")
+    {
+        if (context.WebSockets.IsWebSocketRequest)
+        {
+            using var webSocket = await context.WebSockets.AcceptWebSocketAsync();
+            await SendCurrentTime(webSocket);
+        }
+        else
+        {
+            context.Response.StatusCode = 400;
+        }
+    }
+    else
+    {
+        await next();
+    }
+});
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+app.MapRazorPages()
+   .WithStaticAssets();
+
+app.Run();
+
+static async Task SendCurrentTime(WebSocket webSocket)
+{
+    var buffer = new byte[1024 * 4];
+    while (webSocket.State == WebSocketState.Open)
+    {
+        var currentTime = DateTime.Now.ToString("HH:mm:ss");
+        var bytes = Encoding.UTF8.GetBytes(currentTime);
+        var segment = new ArraySegment<byte>(bytes);
+
+        await webSocket.SendAsync(segment, WebSocketMessageType.Text, endOfMessage: true, CancellationToken.None);
+        await Task.Delay(TimeSpan.FromSeconds(1));
+    }
+}
---
+++ WebSocketDemo/wwwroot/js/site.js
@@ -0,0 +1,16 @@
+﻿function setupWebSocket(targetElement) {
+    const socket = new WebSocket("wss://" + window.location.host + "/ws");
+
+    socket.onopen = function (event) {
+        console.log("WebSocket opened.");
+    };
+
+    socket.onmessage = function (event) {
+        console.log("WebSocket message received:", event.data);
+        targetElement.textContent = event.data;
+    };
+
+    socket.onclose = function (event) {
+        console.log("WebSocket closed.");
+    };
+}
